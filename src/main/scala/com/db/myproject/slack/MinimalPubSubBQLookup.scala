739903f2a0033622c6adf72d22d612879d975a59
albertols <albertols87@gmail.com>
2023-08-08 00:03:45 +0200

    adds some tested windowing for MinimalPubSubBQLookup

--- src/main/scala/com/db/myproject/slack/MinimalPubSubBQLookup.scala
+++ src/main/scala/com/db/myproject/slack/MinimalPubSubBQLookup.scala
@@ -1,19 +1,23 @@
 package com.db.myproject.slack
 
-import com.db.pwcclakees.model.tables.KW111T.KW111TF
-import com.spotify.scio.bigquery.{bigQueryScioContextOps, BigQueryType, Table}
+import com.spotify.scio.bigquery.{BigQueryType, Table, bigQueryScioContextOps}
 import com.spotify.scio.pubsub.PubsubIO
-import com.spotify.scio.streaming.{DISCARDING_FIRED_PANES}
+import com.spotify.scio.streaming.DISCARDING_FIRED_PANES
+import com.spotify.scio.values.SCollection
 import com.spotify.scio.{ContextAndArgs, ScioContext}
 import org.apache.beam.sdk.transforms.windowing._
-import org.checkerframework.checker.nullness.qual.Nullable
 import org.slf4j.{Logger, LoggerFactory}
 
+/**
+ * background:
+ * 1) main PubSub joins PubSub Side input with FixedWindows: https://spotify.github.io/scio/examples/RefreshingSideInputExample.scala.html
+ * 2) GlobalWindow SideInput for lookups: https://beam.apache.org/documentation/patterns/side-inputs/#slowly-updating-global-window-side-inputs
+ */
 object MinimalPubSubBQLookup {
   val log: Logger = LoggerFactory getLogger getClass.getName
 
   // Massive inserts
-  // bq load --autodetect --source_format=CSV --skip_leading_rows=0 db-dev-apyd-pwcclake-es.CDH_dataset.scio_test gs://europe-west3-db-dev-pwcclak-3c7ede6b-bucket/dags/big.sql
+  // bq load --autodetect --source_format=CSV --skip_leading_rows=0 project.dataset.scio_test gs://my_bucket/massive.sql
   @BigQueryType.toTable
   case class Row(key: String, opt: Option[String])
 
@@ -23,33 +27,42 @@ object MinimalPubSubBQLookup {
     val bqTable: String = ???
     val pubsubTopic: String = ???
 
-    val bigSide_1 = sc
-      .typedBigQuery[Row](Table.Spec(bqTable))
-      .withFixedWindows(org.joda.time.Duration.standardDays(1))
-      .withGlobalWindow()
-    bigSide_1.take(5).map(k => log.info(s"bside=>$k"))
-    val bigSide = bigSide_1.map(row => row.key -> (row.key, row.opt)).asMapSideInput
+    val bigSide_1 = bigSideWindow("fixed", sc.typedBigQuery[Row](Table.Spec(bqTable)))
+    bigSide_1.take(5).map(k => log.info(s"bigSide=>$k"))
+    val bigSide = bigSide_1.map { row => row.key -> (row.key, row.opt) }
+      //.asMapSideInput
+      .asMapSingletonSideInput
 
-    val myRtRows = sc
-      .read(PubsubIO.string(pubsubTopic))(PubsubIO.ReadParam(PubsubIO.Subscription))
-      .withFixedWindows(
-        org.joda.time.Duration.standardSeconds(1),
-        // .withGlobalWindow( // Caused by: java.lang.IllegalArgumentException: Attempted to get side input window for GlobalWindow from non-global WindowFn
-        options = com.spotify.scio.values.WindowOptions(
-          allowedLateness = org.joda.time.Duration.ZERO,
-          trigger = Repeatedly.forever(
-            AfterFirst.of(
-              AfterPane.elementCountAtLeast(1),
-              AfterProcessingTime.pastFirstElementInPane()
-            )
-          ),
-          accumulationMode = DISCARDING_FIRED_PANES
-        )
-      )
+    val myRtRows = mainInputWindow("fixed", sc.read(PubsubIO.string(pubsubTopic))(PubsubIO.ReadParam(PubsubIO.Subscription)))
       .withSideInputs(bigSide)
-      .map((key, bbside) => key -> bbside(bigSide).get(key))
+      .map { (key, bbside) => key -> bbside(bigSide).get(key) }
       .toSCollection
     myRtRows.map(k => log.info(s"join=>$k"))
+
     sc.run()
+  }
+
+  def bigSideWindow(winStrategy: String = "fixed", bigSide: SCollection[Row]) =
+    winStrategy match {
+      case "fixed" => bigSide.withFixedWindows(org.joda.time.Duration.standardDays(1))
+      case _ => bigSide // global
+    }
+
+  def mainInputWindow(winStrategy: String = "fixed", bigSide: SCollection[String]) = {
+    val opt = com.spotify.scio.values.WindowOptions(
+      allowedLateness = org.joda.time.Duration.ZERO,
+      trigger = Repeatedly.forever(
+        AfterFirst.of(
+          AfterPane.elementCountAtLeast(1),
+          AfterProcessingTime.pastFirstElementInPane()
+        )
+      ),
+      accumulationMode = DISCARDING_FIRED_PANES
+    )
+    winStrategy match {
+      case "fixed" => bigSide.withFixedWindows(org.joda.time.Duration.standardSeconds(10), options = opt)
+      case "global" => bigSide.withGlobalWindow(options = opt)
+      case _ => bigSide // global
+    }
   }
 }
