8f2df67162fb1a94cb0ff51438de4e68189f7f25
albertols <albertols87@gmail.com>
2023-08-09 16:16:44 +0200

    adds JavaMinimalPubSubBQLookup

--- src/main/scala/com/db/myproject/slack/MinimalPubSubBQLookup.scala
+++ src/main/scala/com/db/myproject/slack/MinimalPubSubBQLookup.scala
@@ -8,47 +8,53 @@ import com.spotify.scio.{ContextAndArgs, ScioContext}
 import org.apache.beam.sdk.transforms.windowing._
 import org.slf4j.{Logger, LoggerFactory}
 
+import com.spotify.scio.bigquery.{BigQueryType, Table, bigQueryScioContextOps}
+import com.spotify.scio.pubsub.PubsubIO
+import com.spotify.scio.streaming.DISCARDING_FIRED_PANES
+import com.spotify.scio.values.SCollection
+import com.spotify.scio.{ContextAndArgs, ScioContext}
+import org.apache.beam.sdk.transforms.windowing._
+import org.slf4j.{Logger, LoggerFactory}
+
 /**
- * background:
+ * open repo: for scaling issues to SCIO and Beam
+ * https://github.com/albertols/scio-db/blob/develop/src/main/scala/com/db/myproject/slack/MinimalPubSubBQLookup.scala
+ *
+ * Exception: using DataFlowRunner throws "java.lang.ClassCastException" when reading from avro GCS interim files (from BQ)
+ * https://stackoverflow.com/questions/76267480/scio-dataflow-error-message-from-worker-java-lang-classcastexception-class-ca
+ *
+ * Background:
  * 1) main PubSub joins PubSub Side input with FixedWindows: https://spotify.github.io/scio/examples/RefreshingSideInputExample.scala.html
  * 2) GlobalWindow SideInput for lookups: https://beam.apache.org/documentation/patterns/side-inputs/#slowly-updating-global-window-side-inputs
  */
 object MinimalPubSubBQLookup {
   val log: Logger = LoggerFactory getLogger getClass.getName
 
-  // Massive inserts
-  // bq load --autodetect --source_format=CSV --skip_leading_rows=0 project.dataset.scio_test gs://my_bucket/massive.sql
-  @BigQueryType.toTable
-  case class Row(key: String, opt: Option[String])
-
   def main(cmdlineArgs: Array[String]): Unit = {
     val (sc, args) = ContextAndArgs(cmdlineArgs)
     implicit val scioContext: ScioContext = sc
-    val bqTable: String = ???
-    val pubsubTopic: String = ???
+    val bqTable: String = args.getOrElse("bqTable", "your_project.your_dataset.scio_test")
+    val pubSubTopic: String = args.getOrElse("pubSubTopic", "projects/your_project/subscriptions/your_topic")
+    val windowLengthSideInput: Long = args.getOrElse("windowLengthSideInput", "86400").toLong // 1 day
+    val windowStrategySideInput: String = args.getOrElse("windowStrategySideInput", "default")
+    val windowLengthPubSub: Long = args.getOrElse("windowLengthPubSub", "10").toLong
+    val windowStrategyPubSub: String = args.getOrElse("windowStrategyPubSub", "fixed")
 
-    val bigSide_1 = bigSideWindow("fixed", sc.typedBigQuery[Row](Table.Spec(bqTable)))
-    bigSide_1.take(5).map(k => log.info(s"bigSide=>$k"))
-    val bigSide = bigSide_1.map { row => row.key -> (row.key, row.opt) }
-      //.asMapSideInput
-      .asMapSingletonSideInput
+    val windowedBigQuery = getCustomWindow(windowStrategySideInput, sc.typedBigQuery[Row](Table.Spec(bqTable)), org.joda.time.Duration.standardSeconds(windowLengthSideInput))
+    windowedBigQuery.take(5).map(k => log.info(s"BQ lookup=>$k"))
+    val keyedBigSide = windowedBigQuery.map(row => row.key -> (row.key, row.opt)).asMapSideInput
+    // .asMapSingletonSideInput
 
-    val myRtRows = mainInputWindow("fixed", sc.read(PubsubIO.string(pubsubTopic))(PubsubIO.ReadParam(PubsubIO.Subscription)))
-      .withSideInputs(bigSide)
-      .map { (key, bbside) => key -> bbside(bigSide).get(key) }
+    val myRtRows = getCustomWindow(windowStrategyPubSub, sc.read(PubsubIO.string(pubSubTopic))(PubsubIO.ReadParam(PubsubIO.Subscription)), org.joda.time.Duration.standardSeconds(windowLengthPubSub))
+      .withSideInputs(keyedBigSide)
+      .map((key, bbside) => key -> bbside(keyedBigSide).get(key))
       .toSCollection
     myRtRows.map(k => log.info(s"join=>$k"))
 
     sc.run()
   }
 
-  def bigSideWindow(winStrategy: String = "fixed", bigSide: SCollection[Row]) =
-    winStrategy match {
-      case "fixed" => bigSide.withFixedWindows(org.joda.time.Duration.standardDays(1))
-      case _ => bigSide // global
-    }
-
-  def mainInputWindow(winStrategy: String = "fixed", bigSide: SCollection[String]) = {
+  def getCustomWindow[T](winStrategy: String, bigSide: SCollection[T], duration: org.joda.time.Duration) = {
     val opt = com.spotify.scio.values.WindowOptions(
       allowedLateness = org.joda.time.Duration.ZERO,
       trigger = Repeatedly.forever(
@@ -60,9 +66,14 @@ object MinimalPubSubBQLookup {
       accumulationMode = DISCARDING_FIRED_PANES
     )
     winStrategy match {
-      case "fixed" => bigSide.withFixedWindows(org.joda.time.Duration.standardSeconds(10), options = opt)
+      case "fixed" => bigSide.withFixedWindows(duration, options = opt)
       case "global" => bigSide.withGlobalWindow(options = opt)
       case _ => bigSide // global
     }
   }
+
+  // Massive inserts to insert quickly key,opt entries for BQ table
+  // bq load --autodetect --source_format=CSV --skip_leading_rows=0 your_project.your_dataset.scio_test gs://europe-west3-db-dev-pwcclak-3c7ede6b-bucket/dags/big.sql
+  @BigQueryType.toTable
+  case class Row(key: String, opt: Option[String])
 }
